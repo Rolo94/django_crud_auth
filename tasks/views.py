a0337224e1b427cf2129df961f079509521f87fb
Rolando Suarez <rolandoasuarez94@gmail.com>
2023-03-02 18:09:50 -0300

    firts commit

---
+++ tasks/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'home.html', {})
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form' : UserCreationForm
+    })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado éxitosamente')
+            except:
+                return HttpResponse('Nombre de usuario existente')
+        return HttpResponse('Las contraseñass no coinciden')
