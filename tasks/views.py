589643ab8a4d166ba20e16ec973749c281d883fb
Rolando Suarez <rolandoasuarez94@gmail.com>
2023-03-09 11:49:12 -0300

    Aplicacion terminada

--- tasks/views.py
+++ tasks/views.py
@@ -45,7 +45,7 @@ def tasks(request):
 @login_required
 def tasks_completed(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
-    return render(request, 'tasks.html', {'tasks': tasks})
+    return render(request, 'tasks_completed.html', {'tasks': tasks})
 
 @login_required
 def create_task(request):
